78622f16f33919806e0e6ac99c47ec3e20253b6c
Daniel Li <daniellitoc@gmail.com>
2014-04-26 01:03:42 +0800

    序列化部分进行了重构，影响到netty的消息序列化部分和memcached序列化部分。

--- src/main/java/org/danielli/xultimate/JavaEncrypt.java
+++ src/main/java/org/danielli/xultimate/JavaEncrypt.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.danielli.xultimate.util.StringUtils;
 import org.danielli.xultimate.util.crypto.CipherUtils;
 import org.danielli.xultimate.util.crypto.SymmetricAlgorithms;
 import org.danielli.xultimate.util.io.FileUtils;
@@ -16,28 +15,28 @@ public class JavaEncrypt {
 	public static void main(String[] args) throws IOException {
 		File parentDirectory = new File("/home/toc/Documents/git/xultimate-crawler/src/test/java/alibaba");
 		// 加密
-//		for (File file :parentDirectory.listFiles()) {
-//			if (file.isDirectory()) {
-//				continue;
-//			}
-//			FileInputStream tmpInput = FileUtils.openInputStream(new File(parentDirectory, file.getName()));
-//			FileOutputStream tmpOutput = FileUtils.openOutputStream(new File(parentDirectory, "Encrypt" + file.getName()));
-//			CipherUtils.encrypt(SymmetricAlgorithms.AES.getCipher(), SymmetricAlgorithms.AES.getKey(""), tmpInput, tmpOutput);
-//			IOUtils.closeQuietly(tmpInput);
-//			IOUtils.closeQuietly(tmpOutput);
-//		}
-		
-		// 解密
 		for (File file :parentDirectory.listFiles()) {
 			if (file.isDirectory()) {
 				continue;
 			}
 			FileInputStream tmpInput = FileUtils.openInputStream(new File(parentDirectory, file.getName()));
-			FileOutputStream tmpOutput = FileUtils.openOutputStream(new File(parentDirectory, StringUtils.replace(file.getName(), "Encrypt", "")));
-			CipherUtils.decrypt(SymmetricAlgorithms.AES.getCipher(), SymmetricAlgorithms.AES.getKey(""), tmpInput, tmpOutput);
+			FileOutputStream tmpOutput = FileUtils.openOutputStream(new File(parentDirectory, "Encrypt" + file.getName()));
+			CipherUtils.encrypt(SymmetricAlgorithms.AES.getCipher(), SymmetricAlgorithms.AES.getKey(""), tmpInput, tmpOutput);
 			IOUtils.closeQuietly(tmpInput);
 			IOUtils.closeQuietly(tmpOutput);
 		}
+		
+		// 解密
+//		for (File file :parentDirectory.listFiles()) {
+//			if (file.isDirectory()) {
+//				continue;
+//			}
+//			FileInputStream tmpInput = FileUtils.openInputStream(new File(parentDirectory, file.getName()));
+//			FileOutputStream tmpOutput = FileUtils.openOutputStream(new File(parentDirectory, StringUtils.replace(file.getName(), "Encrypt", "")));
+//			CipherUtils.decrypt(SymmetricAlgorithms.AES.getCipher(), SymmetricAlgorithms.AES.getKey(""), tmpInput, tmpOutput);
+//			IOUtils.closeQuietly(tmpInput);
+//			IOUtils.closeQuietly(tmpOutput);
+//		}
 	}
 	
 }
